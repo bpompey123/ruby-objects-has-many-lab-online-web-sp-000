684739ab53a1fdac5ce84fd4d2db579548c8f89b
Brian Pompey <brianthescholar@gmail.com>
2019-10-03 23:06:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,6 +6,16 @@ class Author
     @posts = []
   end
 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
 
 
 
