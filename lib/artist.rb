78d128fe8740de006d684cee4087633e15ca2907
Brian Pompey <brianthescholar@gmail.com>
2019-10-03 22:46:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,14 +7,14 @@ class Artist
   end
 
   def add_song(song)
-#    @songs << song
     song.artist = self
   end
 
   def songs
-  #  @songs
     Song.all.select {|song| song.artist == self}
   end
+
+  def add_song_by_name
 
 
 
